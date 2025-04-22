85fed2300019ba97cb6b4ac560350087f6612718
Andres Collazos <afcollazosr@gmail.com>
2025-04-22 15:55:21 +0200

    Extra analysis: running the DNA Masking using the predefined bryophyta library

--- code/05_masking/03_species.sh
+++ code/05_masking/03_species.sh
@@ -3,22 +3,22 @@
 #SBATCH -A uppmax2025-3-3
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 4
+#SBATCH -n 8
 #SBATCH -t 2:00:00
-#SBATCH -J custom_masking_library
+#SBATCH -J masking_with_dfam_lib
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user andres-felipe.collazos-rozo.6881@student.uu.se
-#SBATCH --output=/home/anco6881/genome_analysis/GenomeAnalysis/analyses/05_masking/%x.%j.out
+#SBATCH --output=/home/anco6881/genome_analysis/GenomeAnalysis/analyses/05_masking/03_masking_with_species/%x.%j.out
 
 module load bioinfo-tools
 module load RepeatMasker/4.1.5
 
 export SRC_DIR="/home/anco6881/genome_analysis/GenomeAnalysis"
-export JOB_DIR="$SRC_DIR/analyses/05_masking"
+export JOB_DIR="$SRC_DIR/analyses/05_masking/03_masking_with_species"
 export ASSEMBLY="$SRC_DIR/data/polished_assembly/polished_assembly.fasta"
 
-mkdir -p $JOB_DIR
-cd $JOB_DIR
+mkdir -p "$JOB_DIR"
+cd "$JOB_DIR"
 
 RepeatMasker \
   -species "Bryophyta" \  # From Dfam
@@ -27,5 +27,5 @@ RepeatMasker \
   -no_is \                # Skip bacterial insertion elements check
   -gff \                  # Output GFF3 format annotation file
   -dir $JOB_DIR \          
-  $ASSEMBLY
+  "$ASSEMBLY"
 
