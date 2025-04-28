4e4c50aa9ecd94997e5e9b474733e13c45292079
Andres Collazos <afcollazosr@gmail.com>
2025-04-28 14:58:16 +0200

    Get transcripts in FASTA format from the gff3 from Braker

---
+++ code/08_functional_annotation/gff_to_transcripts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 01:00:00
+#SBATCH -J struc_annotation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=andres-felipe.collazos-rozo.6881@student.uu.se
+#SBATCH --output=/home/anco6881/genome_analysis/GenomeAnalysis/analyses/08_functional_annotation/01_get_transcripts/%x.%j.out
+
+# Load Modules
+module load bioinfo-tools gffread/0.12.7
+
+SRC_DIR="/home/anco6881/genome_analysis/GenomeAnalysis"
+JOB_DIR="$SRC_DIR/analyses/08_functional_annotation/01_get_transcripts/"
+
+# Set input files
+GFF3_FILE="$SRC_DIR/analyses/07_structural_annotation/braker.gff3"
+GENOME_FILE="$SRC_DIR/data/polished_assembly/polished_assembly.fasta"
+
+# Extract transcript sequences
+gffread -w "${JOB_DIR}/braker_transcripts.fa" -g "$GENOME_FILE" "$GFF3_FILE" 
