d7dfff2bfd4b83d7debcfc6ca2dce77850892a6e
Andres Collazos <afcollazosr@gmail.com>
2025-05-03 20:25:45 +0200

    MA Plots and HeatMaps

--- code/10_diff_expression/diff_expression.R
+++ code/10_diff_expression/diff_expression.R
@@ -1,3 +1,5 @@
+setwd('~/Documents/Studies/Genome Analysis/GenomeAnalysis/data/counts/')
+
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
@@ -5,9 +7,16 @@ if (!requireNamespace("DESeq2", quietly = TRUE)) {
   BiocManager::install("DESeq2")
 }
 
-library("DESeq2")
+if (!requireNamespace("apeglm", quietly = TRUE)) {
+  BiocManager::install("apeglm")
+}
 
-setwd('~/Documents/Studies/Genome Analysis/GenomeAnalysis/data/counts/')
+if (!requireNamespace("pheatmap", quietly = TRUE)) {
+  BiocManager::install("pheatmap")
+}
+
+library("DESeq2")
+library("pheatmap")
 
 # Load data
 counts_data <- read.delim("counts.txt", comment.char = "#", stringsAsFactors = FALSE)
@@ -44,8 +53,45 @@ dds <- dds[keep,]
 # Set Factor Level
 dds$condition <- relevel(dds$condition, ref = "control")
 
-dds
-
+# Run DESeq
 dds <- DESeq(dds)
 res <- results(dds)
 res
+resultsNames(dds)
+
+# Shrinkage of effect size (LFC estimates)
+resLFC <- lfcShrink(dds, coef="condition_treatment_vs_control", type="apeglm")
+
+# Summary Results
+summary(res)
+summary(resLFC)
+
+# MA plot
+plotMA(res)
+plotMA(resLFC, ylim=c(-2,2))
+
+# Detect the row number of individual genes by clicking on the plot
+idx <- identify(res$baseMean, res$log2FoldChange)
+rownames(res)[idx]
+
+# Plot counts
+plotCounts(dds, gene=which.min(res$padj), intgroup="condition")
+
+# Heat maps
+ntd <- normTransform(dds)
+vsd <- vst(dds, blind=FALSE)
+rld <- rlog(dds, blind=FALSE)
+
+# Select top 20 expressed genes
+select <- order(rowMeans(counts(dds, normalized=TRUE)), decreasing=TRUE)[1:20]
+
+# Plot heatmaps
+pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=FALSE,
+         cluster_cols=FALSE, annotation_col=coldata)
+
+pheatmap(assay(vsd)[select,], cluster_rows=FALSE, show_rownames=FALSE,
+         cluster_cols=FALSE, annotation_col=coldata)
+
+pheatmap(assay(rld)[select,], cluster_rows=FALSE, show_rownames=FALSE,
+         cluster_cols=FALSE, annotation_col=coldata)
+
