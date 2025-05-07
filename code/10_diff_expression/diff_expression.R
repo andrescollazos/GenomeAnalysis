62f21299aa94635a06818fc5e23118d19d801aaf
Andres Collazos <afcollazosr@gmail.com>
2025-05-07 23:53:29 +0200

    PCA analysis

--- code/10_diff_expression/diff_expression.R
+++ code/10_diff_expression/diff_expression.R
@@ -174,4 +174,16 @@ pheatmap(
 )
 
 
+# PCA
+pcaData <- plotPCA(vsd, intgroup = "condition", returnData = TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+ggplot(pcaData, aes(PC1, PC2, color = condition)) +
+  geom_point(size = 3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  theme_minimal()
+
+
 
