68e62545f5a48c411e948f1c82e1496590698fc2
Andres Collazos <afcollazosr@gmail.com>
2025-05-08 23:00:52 +0200

    Fix path to contigs file

--- code/11_hic_scaffolding/03_juicer.sh
+++ code/11_hic_scaffolding/03_juicer.sh
@@ -15,26 +15,27 @@ module load samtools/1.20
 
 THREADS=8
 
-SRC_DIR="/home/anco6881/genome_analysis/GenomeAnalysis"
+SRC_DIR="/home/anco6881/genome_analysis/GenomeAnalysis/analyses/11_scaffolding/02_scaffolding"
 JOB_DIR="/proj/uppmax2025-3-3/nobackup/work/anco6881/11_scaffolding/03_juicer"
 
-REF_GENOME="$JOB_DIR/polished_assembly.fasta"
-BIN_FILE="$SRC_DIR/analyses/11_scaffolding/02_scaffolding/yahs.out.bin"
-SCAFFOLDS_FINAL="$SRC_DIR/analyses/11_scaffolding/02_scaffolding/yahs.out_scaffolds_final.agp"
+CONTIGS="$SRC_DIR/yahs.out_scaffolds_final.fa"
+BIN_FILE="$SRC_DIR/yahs.out.bin"
+SCAFFOLDS_FINAL="$SRC_DIR/yahs.out_scaffolds_final.agp"
+
+# Step 1: Index the FASTA (needed for AGP parsing and size extraction)
+cd $SRC_DIR
+samtools faidx $CONTIGS
 
 cd $JOB_DIR
 
-# Step 1: Index the FASTA (needed for AGP parsing and size extraction)
-samtools faidx $REF_GENOME
-
 # Step 2: Create sorted alignment file for juicer_tools input
 /proj/uppmax2025-3-3/Genome_Analysis/yahs/juicer pre \
-    $BIN_FILE $SCAFFOLDS_FINAL polished_assembly.fasta.fai | \
+    $BIN_FILE $SCAFFOLDS_FINAL ${CONTIGS}.fai | \
     sort -k2,2d -k6,6d -T ./ --parallel=$THREADS -S32G | \
     awk 'NF' > alignments_sorted.txt
 
 # Step 3: Extract scaffold sizes from the FASTA index
-cut -f1,2 polished_assembly.fasta.fai > scaffolds_final.chrom.sizes
+cut -f1,2 ${CONTIGS}.fai > scaffolds_final.chrom.sizes
 
 # Step 4: Generate the .hic file for Juicebox visualization
 java -Xmx32G -jar /proj/uppmax2025-3-3/Genome_Analysis/yahs/juicer_tools_1.22.01.jar pre \
