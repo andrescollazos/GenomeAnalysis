9e7b1ebbf5c1f5fc88c6578e0e3cd3e52b62eb14
Andres Collazos <afcollazosr@gmail.com>
2025-05-10 21:53:02 +0200

    - Use hyperGTest to do the enrichment of GO terms. - Provide this as input for rrvgo - Create scatterplots - HeatmapPlot - TreemapPlot

--- code/12_gene_enrichment/gene_enrichment.R
+++ code/12_gene_enrichment/gene_enrichment.R
@@ -5,9 +5,28 @@ if (!requireNamespace("rrvgo", quietly = TRUE)) {
   BiocManager::install("rrvgo")
 }
 
+if (!requireNamespace("org.At.tair.db", quietly = TRUE)) {
+  BiocManager::install("org.At.tair.db")
+}
+
+if (!requireNamespace("GOstats", quietly = TRUE)) {
+  BiocManager::install("GOstats")
+}
+
+if (!requireNamespace("GSEABase", quietly = TRUE)) {
+  BiocManager::install("GSEABase")
+}
+
+
+
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(tibble)
+library("rrvgo")
+library(GOstats)
+library(GSEABase)
+
 
 setwd('~/Documents/Studies/Genome Analysis/GenomeAnalysis/data/gene_enrichment/')
 
@@ -30,12 +49,73 @@ go_table <- gff %>%
 colnames(go_table) <- c("gene", "go_id")
 go_table <- go_table %>% filter(!is.na(go_id))
 
+# ------------------------------------------------------------------------------
 # Load Differential Expression Results
 res_df <- read.csv("deseq2_results.csv", row.names = 1)
 sig_genes <- rownames(res_df %>% filter(padj < 0.05 & abs(log2FoldChange) > 1))
 
-# Match with GO-annotated genes
-sig_go_table <- go_table %>% filter(gene %in% sig_genes)
+gene_universe <- rownames(res_df)
+gene_ids <- sig_genes
+
+
+
+# Filter and create clean base R data.frame
+go_df <- data.frame(
+  go_id = as.character(go_table$go_id),
+  Evidence = "IEA",
+  gene_id = as.character(go_table$gene),
+  stringsAsFactors = FALSE
+)
+goframe <- GOFrame(go_df, organism = "Niphotrichum japonicum")
+goallframe <- GOAllFrame(goframe)
+
+# Make geneSetCollection object
+gsc <- GeneSetCollection(goallframe, setType = GOCollection())
+
+params <- GSEAGOHyperGParams(
+  name = "GOstats enrichment",
+  geneSetCollection = gsc,
+  geneIds = gene_ids,
+  universeGeneIds = gene_universe,
+  ontology = "BP",
+  pvalueCutoff = 0.05,
+  conditional = FALSE,
+  testDirection = "over"
+)
+
+go_result <- hyperGTest(params)
+go_table_stats <- summary(go_result)
+write.csv(go_table_stats, "gostats_enrichment.csv", row.names = FALSE)
+
+simMatrix <- calculateSimMatrix(
+  go_table_stats$GOBPID,
+  orgdb = "org.At.tair.db",
+  ont = "BP",
+  method = "Rel"
+)
+scores <- setNames(-log10(go_table_stats$Pvalue), go_table_stats$GOBPID)
+reducedTerms <- reduceSimMatrix(
+  simMatrix,
+  scores,
+  orgdb = "org.At.tair.db",
+  threshold = 0.7
+)
+scatterPlot(simMatrix, reducedTerms, addLabel = TRUE, onlyParents = TRUE)
+scatterPlot(simMatrix, reducedTerms, addLabel = TRUE)
+
+heatmapPlot(
+  simMatrix,
+  reducedTerms,
+  annotateParent=TRUE,
+  annotationLabel="parentTerm",
+  fontsize=6
+)
+
+treemapPlot(reducedTerms)
+wordcloudPlot(reducedTerms, min.freq=1, colors="black")
+
+
+
 
 
 
