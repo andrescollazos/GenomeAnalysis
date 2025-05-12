18781c5a3802190e9b4014f955da13c915f88821
Andres Collazos <afcollazosr@gmail.com>
2025-05-12 11:11:03 +0200

    Increase cores

--- code/04_evaluation/03_QUAST_polished_assembly.sh
+++ code/04_evaluation/03_QUAST_polished_assembly.sh
@@ -3,9 +3,9 @@
 #SBATCH -A uppmax2025-3-3
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 30:00
-#SBATCH -J quast_unpolished
+#SBATCH -J quast_polished
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user andres-felipe.collazos-rozo.6881@student.uu.se
 #SBATCH --output=/home/anco6881/genome_analysis/GenomeAnalysis/analyses/04_evaluation/03_QUAST_polished_assembly/%x.%j.out
@@ -21,7 +21,7 @@ cd $JOB_DIR
 
 python /sw/bioinfo/quast/5.0.2/rackham/bin/quast.py $ASSEMBLY \
   --eukaryote \
-  --threads 2 \
+  --threads 4 \
   --large \
   --labels Polished \
   -o $JOB_DIR
